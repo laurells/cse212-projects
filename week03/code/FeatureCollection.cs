18afe7c776f83665e66a6f9a600fb8e9684c80be
Laurels Echichinwo <laurels.echichinwo@stu.cu.edu.ng>
2024-07-12 13:26:17 +0100

    Update FeatureCollection.cs

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,4 +1,19 @@
+
 public class FeatureCollection {
     // Todo Earthquake Problem - ADD YOUR CODE HERE
     // Create additional classes as necessary
+    public string Type { get; set; }
+    public List<Feature> Features { get; set; }
+}
+
+public class Feature
+{
+    public string Type { get; set; }
+    public Properties Properties { get; set; }
+}
+
+public class Properties
+{
+    public string Place { get; set; }
+    public double Mag { get; set; }
 }
