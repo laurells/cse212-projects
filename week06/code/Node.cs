3c9fd1c92a0801e2fae45d7b1c04b4afe2761b63
Laurels Echichinwo <laurels.echichinwo@stu.cu.edu.ng>
2024-07-23 15:25:21 +0100

    Update Node.cs

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -15,7 +15,7 @@ public class Node {
             else
                 Left.Insert(value);
         }
-        else {
+        else if (value > Data){
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
@@ -26,11 +26,29 @@ public class Node {
 
     public bool Contains(int value) {
         // TODO Start Problem 2
+        if (value == Data) {
+            return true;
+        }
+        else if (value < Data && Left is not null) {
+            return Left.Contains(value);
+        }
+        else if (value > Data && Right is not null) {
+            return Right.Contains(value);
+        }
         return false;
     }
 
     public int GetHeight() {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        // return 0; // Replace this line with the correct return statement(s)
+        if (this == null) {
+            return 0;
+        }
+
+        int leftHeight = (Left == null) ? 0 : Left.GetHeight();
+        int rightHeight = (Right == null) ? 0 : Right.GetHeight();
+
+        // Return height of the current node
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
